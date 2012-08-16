95370c7620da27cbe94d74f79801564c299e1e45
yellow1912 <yellow1912@gmail.com>
2012-08-16 23:21:08 +0700

    minor updates

--- lib/Listing.php
+++ lib/Listing.php
@@ -2,23 +2,16 @@
 namespace plugins\riSingleListing;
 
 class Listing{
-	public function render($listing_split, $template = 'riSingleListing::_product.php'){  
+	public function render($listing_split, $template = 'riSingleListing::_product.php'){
 	    global $db, $riview;      
 	    $content = '';
-        if ($listing_split->number_of_rows > 0) {
-            $products = $db->Execute($listing_split->sql_query);
-            while (!$products->EOF) {        
-                $product = $products->fields;
-                $product = array_merge($product,
-                    array(
-            			'products_display_price' => zen_get_products_display_price($products->fields['products_id']),    			            			
-            			'products_link' => zen_href_link(zen_get_info_page($product['products_id']), 'products_id=' . $product['products_id'])                        
-                    )
-                );
-                $content .= $riview->render($template, $product);
-                $products->MoveNext();
-            }
+        
+        $products = $this->getProducts($listing_split);
+        while (foreach $products as $product) {                    
+            $content .= $riview->render($template, $product);
+            $products->MoveNext();
         }
+        
         return $content;
-	}
+	}	
 }
